afadd74f0ea915a0140da8c69a5047a6f437340e
mikep-png <ssedccarn@gmail.com>
2026-07-28 16:29:06 -0500

    Se añade lo trabajo el dia de hoy, Actividad clase 3 — Agenda ADSO con componentes reutilizables y props

--- agenda-adso/src/ejercicio.js
+++
@@ -1,7 +0,0 @@
-const aprendiz = {
-    nombre: "Gustavo",
-    ficha: 3223874,
-}
-const notas = [4.0, 3.8, 1.8]
-const promedio = (notas[0] + notas[1] + notas[2]) / 3;
-console.log(promedio);
--- agenda-adso/src/ejercicio3.js
+++
@@ -1,7 +0,0 @@
-const aprendiz = {
- nombre: "Gustavo",
- ficha: 3223874,
- programa: "ADSO"
-};
-console.log(aprendiz.nombre); // Gustavo
-console.log(aprendiz["ficha"]); // 3223874
